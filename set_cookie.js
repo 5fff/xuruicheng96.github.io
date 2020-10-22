5c254fe7d34b33a2f7324871661d3e3fe48cb7df
Ray <xuruicheng96@gmail.com>
2020-10-22 10:42:28 -0500

    add domain to js

--- set_cookie.js
+++ set_cookie.js
@@ -1,8 +1,4 @@
 function setCookie() {
-    var cookieName = "HelloWorld";
-    var cookieValue = "HelloWorld";
-    var myDate = Date();
-    myDate.setMonth(myDate)
     document.cookie = "localhostcookie=hellolocalhost;Domain=localhost";
     console.log(document.cookie);
 }
