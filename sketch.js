5f5f306a1be111991530437a330c58a5d1512009
Ray <xuruicheng96@gmail.com>
2020-12-18 19:03:47 -0600

    update

--- sketch.js
+++ sketch.js
@@ -2,10 +2,16 @@ crsCookieManager.cookieData = {
   env: "POC",
   cookies: [
     {
-     	 
+        name: "cookiePref",
+        value: "FUN"
     },
     {
-      
+        name: "cookiePref2",
+        value: "FUNddfd"
     }
   ]
-}
+};
+
+
+
+crsCookieManager.updateCookie()
